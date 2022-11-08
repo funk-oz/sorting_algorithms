6da8946530fb692641524ce30893395e7b3dc271
mwangizzle <62013376+funk-oz@users.noreply.github.com>
2022-11-08 14:36:53 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,34 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
+ * bubble_sort - Sort array of integers using bubble sort
+ * @array: pointer to integer array
  * @size: array size
+ * Return: Nothing
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, tmp, len;
+	size_t swapped = 1;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+
+	len = size;
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[i] > array[i + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				swapped = 1;
 			}
 		}
+		len--;
+	}
 }
